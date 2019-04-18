a64f9eb9beaa036f9abcac9ebc528b5d7dec8344
aitor <aitor@e451.net>
2019-04-18 10:08:47 +0200

    arrow keys binding for slider

--- web/app/themes/anv/resources/assets/scripts/routes/home.js
+++ web/app/themes/anv/resources/assets/scripts/routes/home.js
@@ -2,6 +2,8 @@ export default {
   init() {
     // JavaScript to be fired on the home page
 
+    // Slick carousel
+
     let width = $(window).width();
 
     function posicionar() {
@@ -21,7 +23,6 @@ export default {
       $('.container-grid .grid').slick({
         arrows: false,
         autoplay: true,
-        focusOnChange: true,
         fade: true,
         speed: 2000,
         autoplaySpeed: 5000,
@@ -29,12 +30,34 @@ export default {
 
       $('.slick-current').focus();
 
-      // posicionar carrusel
       posicionar();
 
       $(window).resize(function() {
         posicionar();
       });
+
+
+
+      $(document).keydown(function(e) {
+        switch (e.which) {
+          case 37: // left
+            $('.container-grid .grid').slick('slickPrev');
+            console.log('prev');
+            break;
+
+          case 39: // right
+            $('.container-grid .grid').slick('slickNext');
+            console.log('next');
+            break;
+
+          default:
+            return; // exit this handler for other keys
+        }
+        e.preventDefault(); // prevent the default action (scroll / move caret)
+      });
+
+
+
 
     }
 
