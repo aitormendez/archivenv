79b63b17fdb857a44dc4ce2b29da77b2c1ac3c2c
aitor <aitor@e451.net>
2019-04-21 19:17:22 +0200

    PHP error fix

--- web/app/themes/anv/app/Controllers/TaxonomySerie.php
+++ web/app/themes/anv/app/Controllers/TaxonomySerie.php
@@ -8,7 +8,7 @@ class TaxonomySerie extends Controller
 {
   public static function image() {
 
-    $post_thumbnail_id = get_post_thumbnail_id($post->ID);
+    $post_thumbnail_id = get_post_thumbnail_id();
     $object_id = apply_filters( 'wpml_object_id', $post_thumbnail_id, 'attachment', FALSE, NULL );
     $miniatura_traducida = get_post($object_id);
     $attachment_alt = get_post_meta($object_id, '_wp_attachment_image_alt', true);
