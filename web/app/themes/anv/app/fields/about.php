92117815e6442017c5ea7910d3ea0cdc578b982c
aitor <aitor@e451.net>
2019-04-23 17:52:01 +0200

    restringir algunos meta fields a page about

--- web/app/themes/anv/app/fields/about.php
+++ web/app/themes/anv/app/fields/about.php
@@ -4,10 +4,16 @@ namespace App;
 
 use StoutLogic\AcfBuilder\FieldsBuilder;
 
+$page_en = get_page_by_title('About');
+$page_ca = get_page_by_title('Sobre l’Arxiu Navarro Vives');
+$page_es = get_page_by_title('Sobre Archivo Navarro Vives');
+
 $about = new FieldsBuilder('about');
 
 $about
-    ->setLocation('post_type', '==', 'page');
+	->setLocation('page', '==', $page_en->ID)
+		->or('page', '==', $page_ca->ID)
+		->or('page', '==', $page_es->ID);
   
 $about
 	->addRepeater('chapters')
