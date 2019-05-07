1c2ee7aa0eda7aaa4b2f8d871f330299520ff399
aitor <aitor@e451.net>
2019-05-07 17:28:20 +0200

    Corrección CSS bio h2

--- web/app/themes/anv/resources/views/partials/content-bio.blade.php
+++ web/app/themes/anv/resources/views/partials/content-bio.blade.php
@@ -1,7 +1,7 @@
 <article @php post_class( 'montserrat mb-8')
 @endphp>
   <header>
-    <h2 class="entry-title text-lg">{!! get_the_title() !!}</h2>
+    <h2 class="entry-title text-lg mt-0">{!! get_the_title() !!}</h2>
   </header>
   <div class="keep">
     @php the_content() @endphp
